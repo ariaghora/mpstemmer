cb74332c6d7d0153ada544f31260dcb8b0b1264d
Aria Ghora Prabono <hello@ghora.net>
2021-01-19 17:36:04 +0900

    restructure the checking layers

--- mpstemmer.py
+++ mpstemmer.py
@@ -3,14 +3,16 @@ import editdistance
 import csstemmer
 
 """
-kumpulan kata dari KBBI daring
+kumpulan kata dari KBBI daring. Gunakan set karena lebih cepat daripada list untuk method
+`__contains()__`.
 """
-KOSAKATA = [kata.lower() for kata in open('kbbi_words.txt', 'r').read().split('\n')]
+KOSAKATA = {kata.lower() for kata in open('kbbi_words.txt', 'r').read().split('\n')}
 
 """
 kumpulan kata tak baku yang lazim ditemui
 """
 COMMON_INFORMAL = json.loads(open('./common_informal.json', 'r').read())
+
 
 def get_top_n_matching(kata, n):
     """
@@ -45,76 +47,121 @@ def fix_common(kata):
     return res, fixed
 
 
-def proc_suffix_in(kata):
-    res = kata
-    if kata.endswith('in'):
-        res = kata[:-2]
-    return res
-
-
 def proc_prefix_ng(kata):
     res = kata
 
 
-def proc_affix_ng_in(kata):
+def check_nonstandard_affixed(kata):
     """
-    Stemming kata dengan pola (ngu|nge|ng)+word+in. Postfix -i diasumsikan tertangani via similarity search.
+    Heuristik cepat dan sederhana untuk menentukan baku-tidaknya suatu kata.
+    Pastikan sebelumnya kata memang tidak ditemukan di kosakata KBBI (dengan pencarian eksak).
     :param kata:
     :return:
     """
     res = kata
+    maybe_nonstandard = False
+    if res.startswith(('m', 'n', 'ng', 'ny', 'ke')) or res.endswith(('i', 'in', 'an')):
+        maybe_nonstandard = True
 
-    if kata.startswith('nge') and kata.endswith('in'):
-        """ 
-        Langsung buang afiks dan kembalikan hasilnya jika kemungkinan tidak terjadi overstemming (len(kata) > 4). 
-        Kesalahan akan diakomodasi similarity search di tahap selanjutnya. 
-        """
-        res = kata[3:-2]
-        if len(res) >= 4:
-            return res
+    return maybe_nonstandard
 
-    elif kata.startswith('ng') and kata.endswith('in'):
-        res = '-' + kata[2:-2]
 
-    elif kata.startswith('ng') and kata.endswith('i'):
-        res = '-' + kata[2:-1]
+def ensure_standard_root(kata, kosakata):
+    """
+    Kata diasumsikan tidak baku. Harus dilakukan pemeriksaan terlebih dahulu sebelum menggunakan fungsi ini.
+    Pastikan suffix telah dibuang terlebih dahulu.
+    Fungsi ini melakukan perbaikan minor untuk kata-kata seperti, sebel->sebal, (se|menye|nye)sel->nyesal.
+    :param kata:
+    :param kosakata:
+    :return:
+    """
+    res = kata
+    if not (res[-1] in 'aiueo') and (res[-2] == 'e'):
+        case1 = res
+        case2 = case1[:-2] + 'a' + case1[-1:]
 
-    elif kata.startswith('nge'):
-        res = '-' + kata[3:]
+        if case1 in kosakata:
+            res = case1
+        elif case2 in kosakata:
+            res = case2
 
     return res
 
 
-def stem_kata(kata):
+def fix_nonstandard_prefix(kata):
+    """
+    Kata diasumsikan tidak baku. Harus dilakukan pemeriksaan terlebih dahulu sebelum menggunakan fungsi ini.
+    Fungsi ini tidak benar-benar memperbaiki suffix hingga memperoleh bentuk valid, a.l., ngeberesin --> mengeberes.
+    Stemming lanjutan akan ditangani oleh stemmer standar, a.l., confix-stripping, dll.
+    :param kata:
+    :return:
+    """
+    res = kata
+    if res.startswith(('m', 'n', 'ng', 'ny')):
+        res = 'me' + res
+
+    return res
+
+
+def fix_nonstandard_suffix(kata):
+    res = kata
+    # buang suffix sederhana
+    if res.endswith('in'):
+        res = res[:-2]
+    elif res.endswith('i'):
+        res = res[:-1]
+    return res
+
+
+def stem_kata(kata, kosakata, rigor=False):
     """
     :param kata: kata tak baku.
     :return: kata yang dibakukan.
     """
     res = kata
 
-    res, fixed = fix_common(res)
-    if fixed:
-        return res
-
-    """ Lapis 1: cari di KBBI (eksak). """
+    """ 
+    Lapis 1: cari di KBBI (eksak). 
+    """
     if res in KOSAKATA:
         return res
 
-    """ Lapis 2: jika tidak ditemukan, ada kemungkinan kata masih terafiksasi. Lakukan stemming. """
-    # TODO: implementasi stemmer untuk kata baku. Heeeelp.
-    # res = stem_formal(res)
+    """ 
+    Lapis 2: cek kemungkinan kata tak baku terafiksasi. Jika ya, bakukan afiksasi.
+    """
+    maybe_nonstandard = check_nonstandard_affixed(res)
+    if maybe_nonstandard:
+        res = fix_nonstandard_suffix(res)
+        if res in kosakata:
+            return res
+        res = fix_nonstandard_prefix(res)
+        if res in kosakata:
+            return res
 
-    """ Lapis 3: jika tidak ditemukan, ada kemungkinan kata tidak baku. Lakukan pembakuan. """
-    res = proc_affix_ng_in(res)
-    res = proc_suffix_in(res)
+    """ 
+    Lapis 3: Setelah lapis 2, ada kemungkinan kata masih terafiksasi. Lakukan stemming standar,
+    karena afiksasi telah dibakukan di lapis 2. 
+    """
+    res = csstemmer.stem(res, kosakata)
 
-    """ Lapis 4: cari di KBBI (berdasarkan kemiripan/tak eksak). """
-    res = get_top_1_matching(res)
+    """ 
+    Lapis 4: Hasil lapis 3 belum tentu memperoleh akar kata baku. Karena itu, jika sebelumnya kata telah
+    terindikasi tidak baku, pastikan akar kata dibakukan. 
+    """
+    if maybe_nonstandard:
+        res = ensure_standard_root(res, kosakata)
+
+    """ 
+    Lapis 5: Opsional. Pencarian lebih detail melalui similarity search. 
+    PERINGATAN!!! Ini bisa memakan banyak waktu. 
+    """
+    if rigor:
+        res = get_top_1_matching(res)
 
     return res
 
 
-def stem_kalimat(kalimat):
+def stem_kalimat(kalimat, kosakata):
     """
     :param kalimat: Kalimat (baris kata terpisah spasi) yang akan dibakukan.
     Sementara ini, karakter non-alfanumerik akan dihilangkan.
@@ -123,29 +170,34 @@ def stem_kalimat(kalimat):
     res = []
     words = kalimat.split(' ')
     for kata in words:
-        res.append(stem_kata(kata))
+        res.append(stem_kata(kata, kosakata))
     return ' '.join(res)
 
 
 if __name__ == '__main__':
     kalimat_raw = 'ngerampok tuh dosa bang, apa ga takut?'
-    kalimat_stem = stem_kalimat(kalimat_raw)
+    kalimat_stem = stem_kalimat(kalimat_raw, KOSAKATA)
     print(f'           raw : {kalimat_raw}')
     print(f'hasil stemming : {kalimat_stem}')
 
     kalimat_raw = 'ngelupain mantan tuh nggak sulit kok'
-    kalimat_stem = stem_kalimat(kalimat_raw)
+    kalimat_stem = stem_kalimat(kalimat_raw, KOSAKATA)
     print(f'           raw : {kalimat_raw}')
     print(f'hasil stemming : {kalimat_stem}')
 
-    print(csstemmer.stem('nilaiku', KOSAKATA))
-    print(csstemmer.stem('berai', KOSAKATA))
-    print(csstemmer.stem('bukankah', KOSAKATA))
+    print(stem_kata('nilaiku', KOSAKATA))
+    print(stem_kata('berai', KOSAKATA))
+    print(stem_kata('bukankah', KOSAKATA))
 
-    print(csstemmer.stem('bercerita', KOSAKATA))
-    print(csstemmer.stem('berlarian', KOSAKATA))
-    print(csstemmer.stem('belajar', KOSAKATA))
-    print(csstemmer.stem('beterbangan', KOSAKATA))
+    print(stem_kata('bercerita', KOSAKATA))
+    print(stem_kata('berlarian', KOSAKATA))
+    print(stem_kata('belajar', KOSAKATA))
+    print(stem_kata('beterbangan', KOSAKATA))
 
-    print(csstemmer.stem('terangkat', KOSAKATA))
-    print(csstemmer.stem('terundung', KOSAKATA))
+    print(stem_kata('terangkat', KOSAKATA))
+    print(stem_kata('terundung', KOSAKATA))
+
+    print(stem_kata('kuberesin', KOSAKATA))
+    print(stem_kata('ngancem', KOSAKATA))
+    print(stem_kata('nyesel', KOSAKATA))
+    print(stem_kata('ngelurusin', KOSAKATA))
