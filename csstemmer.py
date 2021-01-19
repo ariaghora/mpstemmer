9d693fb22a206c020dd915eb2403809827003a7c
Aria Ghora Prabono <hello@ghora.net>
2021-01-19 17:35:27 +0900

    reformat code

--- csstemmer.py
+++ csstemmer.py
@@ -4,14 +4,15 @@ Confix-stripping stemmer, Adriani et al., 2007
 [[[DP+]DP+]DP+] root-word [[+DS][+PP][+P]]
 """
 
-VOWELS = 'aiueo'
 
 def is_vowel(c):
-    return c in VOWELS
+    return c in 'aiueo'
+
 
 def is_consonant(c):
     # faster than checking 21 other characters
-    return not(c in VOWELS)
+    return not (c in 'aiueo')
+
 
 def is_in_dict(kata, kosakata):
     return kata in kosakata
@@ -20,7 +21,7 @@ def is_in_dict(kata, kosakata):
 def remove_inflectional_suffixes(kata, kosakata):
     res = kata
 
-    if is_in_dict(res, kosakata): 
+    if is_in_dict(res, kosakata):
         return res
 
     if res[-3:] in ['kah', 'lah', 'tah', 'pun', 'nya']:
@@ -32,7 +33,7 @@ def remove_inflectional_suffixes(kata, kosakata):
 
 def remove_derivational_suffix(kata, kosakata):
     res = kata
-    if is_in_dict(res, kosakata): 
+    if is_in_dict(res, kosakata):
         return res
 
     if res.endswith('i'):
@@ -47,18 +48,18 @@ def remove_derivational_suffix(kata, kosakata):
 def remove_prefixes(kata, kosakata, n_removed_suffixes, last_removed):
     res = kata
     # base case
-    if is_in_dict(res, kosakata): 
-        return res 
+    if is_in_dict(res, kosakata):
+        return res
 
-    if (res[:2] in ['di', 'ke', 'se']) and (res[:2] != last_removed) and (n_removed_suffixes < 3):
+    if (res[:2] in ['di', 'ke', 'se', 'ku']) and (res[:2] != last_removed) and (n_removed_suffixes < 3):
         # simple prefix, just remove it
         res = remove_prefixes(res[2:], kosakata, n_removed_suffixes + 1, res[:2])
 
     elif (res[:2] == 'be'):
         # rule 1: berV --> ber-V | be-rV
         if (res[2] == 'r') and is_vowel(res[3]):
-            case1 = res[3:] # ber-V
-            case2 = res[2:] # be-rV
+            case1 = res[3:]  # ber-V
+            case2 = res[2:]  # be-rV
             if is_in_dict(case1, kosakata):
                 res = remove_prefixes(case1, kosakata, n_removed_suffixes + 1, res[:2])
             elif is_in_dict(case2, kosakata):
@@ -78,33 +79,36 @@ def remove_prefixes(kata, kosakata, n_removed_suffixes, last_removed):
             res = remove_prefixes(res[3:], kosakata, n_removed_suffixes + 1, res[:2])
 
         # rule 5: beC1erC... --> be-C1erC..., C1 != {'r'|'l'}  (beterbangan)
-        elif not(res[2] in ['r', 'l']) and is_consonant(res[2]) and is_consonant(res[5]):
+        elif not (res[2] in ['r', 'l']) and is_consonant(res[2]) and is_consonant(res[5]):
             res = remove_prefixes(res[2:], kosakata, n_removed_suffixes + 1, res[:2])
-    
+
     elif (res[:2] == 'te'):
         # rule 6: terV... --> ter-V... | te-rV... (terangkat | terundung)
         if (res[2] == 'r') and is_vowel(res[3]):
-            case1 = res[3:] # ter-V
-            case2 = res[2:] # te-rV
+            case1 = res[3:]  # ter-V
+            case2 = res[2:]  # te-rV
             if is_in_dict(case1, kosakata):
                 res = remove_prefixes(case1, kosakata, n_removed_suffixes + 1, res[:2])
             elif is_in_dict(case2, kosakata):
                 res = remove_prefixes(case2, kosakata, n_removed_suffixes + 1, res[:2])
 
+    # TODO: lengkapi semua rules!
+
     return res
+
 
 def stem(kata, kosakata):
     # in some rare occassions, we need to remove prefixes before suffixes
     # to reduce ambiguities
     if (
-        (kata.startswith('be') and kata.endswith('i')) or 
-        (kata.startswith('be') and kata.endswith('lah')) or 
-        (kata.startswith('be') and kata.endswith('an')) or
-        (kata.startswith('me') and kata.endswith('i')) or
-        (kata.startswith('di') and kata.endswith('i')) or
-        (kata.startswith('pe') and kata.endswith('i')) or
-        (kata.startswith('te') and kata.endswith('i'))
-        ):
+            (kata.startswith('be') and kata.endswith('i')) or
+            (kata.startswith('be') and kata.endswith('lah')) or
+            (kata.startswith('be') and kata.endswith('an')) or
+            (kata.startswith('me') and kata.endswith('i')) or
+            (kata.startswith('di') and kata.endswith('i')) or
+            (kata.startswith('pe') and kata.endswith('i')) or
+            (kata.startswith('te') and kata.endswith('i'))
+    ):
         res = remove_prefixes(kata, kosakata, 0, '')
         res = remove_inflectional_suffixes(res, kosakata)
         res = remove_derivational_suffix(res, kosakata)
@@ -113,5 +117,5 @@ def stem(kata, kosakata):
         res = remove_inflectional_suffixes(kata, kosakata)
         res = remove_derivational_suffix(res, kosakata)
         res = remove_prefixes(res, kosakata, 0, '')
-    
+
     return res
